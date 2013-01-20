0fc186f71ca672d2ac8ffb91a84beee36138ed89
Sergio Sanchez Trujillo <trukise@gmail.com>
2013-01-20 19:24:09 +0100

    Clientes (index), Eliminación clientes, nuevo cliente
    En esta nueva versiçn, secrea dinamicamente la tabla de clientes en el
    index, y su correspondiente botón eliminar en la tabla con ventanas
    modales individuales.
    
    También hemos creado la operación nuevo cliente, mediante el helper
    FORM, en principio no parece ir mal.
    
    Hemos insertado también unos DIV de información (azules) que en el caso
    de ser errores adquieren el color rojo.

--- controllers/clientes.py
+++ controllers/clientes.py
@@ -5,7 +5,17 @@ def download(): return response.download(request,db)
 def call(): return service()
 ### end requires
 def index():
-    return dict()
+    clientes = dbRosan().select(dbRosan.cliente.ALL, orderby=dbRosan.cliente.nombre)
+    return dict(clientes=clientes)
+
+
+def eliminar():
+    id = request.args[0]
+    clienteEliminado = dbRosan.cliente(id)
+    nombre = clienteEliminado.nombre
+    dbRosan(dbRosan.cliente.id==id).delete()
+    return dict(id=id, nombre=nombre)
+
     
 
 def clientes():
@@ -14,3 +24,17 @@ def clientes():
         response.flash = 'cliente insertado'
     return dict(form=form)  
   
+
+def nuevoCliente():
+    info = ""
+    errores = ""
+
+    form = SQLFORM(dbRosan.cliente)
+    
+    if form.process().accepted:
+        info = "El cliente ha sido insertado"
+    elif form.errors:
+        errores = "Ha habido errores compruebe los campos"
+    else:
+        info = "Rellene el formulario para introducir un cliente en la base de datos."
+    return dict(form=form, info=info, errores=errores)
