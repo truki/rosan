2b595ba6bf2cc8e1a2de59527538fb8b067ff9f6
Sergio Sanchez Trujillo <trukise@gmail.com>
2013-01-23 00:54:25 +0100

    Creamos el index de artículos y modificamos la botonera izquierda y además arreglamos lo de la fuente "open sans" que no se configuraba como tal
    Creamos Index de artículos.
    
    Modificamos botonera izquierda haciendo que los botones ocupen todo el
    span4. (había que incluir una hoja de estilos la faq.css)
    
    Modificamos el nombre de la aplicación, a Rosan ERP.
    
    Añadimos el enlace correcto a la fuente de Google "Open Sans" para que
    se configure correctamente.

--- controllers/articulos.py
+++ controllers/articulos.py
@@ -4,5 +4,13 @@ def user(): return dict(form=auth())
 def download(): return response.download(request,db)
 def call(): return service()
 ### end requires
+def index():
+	articulos = dbRosan().select(dbRosan.articulo.ALL, orderby=dbRosan.articulo.titulo)
+	return dict(articulos=articulos)
 
-def index(): return dict(message="hello from articulos.py")
+def eliminar():
+	id = request.args[0]
+	articuloEliminado = dbRosan.articulo(id)
+	nombre = articuloEliminado.titulo
+	dbRosan(dbRosan.articulo.id==id).delete()
+	return dict(id=id, nombre=nombre)
