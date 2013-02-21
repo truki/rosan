87ddca71918369302e427a9540702e15be80ade7
Truki <trukise@gmail.com>
2013-02-21 16:22:00 +0100

    Se añade tratamiento del formConcepto en detalleFactura.html
    Se añade tratamiento del formConcepto en detalleFactura.html

--- controllers/facturas.py
+++ controllers/facturas.py
@@ -7,6 +7,7 @@ def index():
 
 # Funcion nuevaFactura: INicio del creación de nueva factura.
 def nuevaFactura():
+
 	return dict()
 
 # Funcion detalleFactura: donde se insertan las lineas de cada factura
@@ -19,17 +20,24 @@ def detalleFactura():
 									  DIV(INPUT(_name="cantidad", _type="text", _class="input-large", _id="cantidad", _value="1", requires=IS_INT_IN_RANGE(1,999999, error_message="Introduce un valor entre 1 y 999999")), _class="controls"), 
 								   _class="control-group"),
 								  DIV(LABEL("Precio", _class="control-label"),
-								  	  DIV(INPUT(_name=",precio", _type="text", _class="input-large", _id="precio", requires=IS_FLOAT_IN_RANGE(0,9999999, dot=".", error_message="Introduce un valor entre 0 y 9999999, el signo de los decimales es el .")), _class="controls"),
+								  	  DIV(INPUT(_name="precio", _type="text", _class="input-large", _id="precio", requires=IS_FLOAT_IN_RANGE(0,9999999, dot=".", error_message="Introduce un valor entre 0 y 9999999, el signo de los decimales es el .")), _class="controls"),
 								   _class="control-group"),
 								  DIV(DIV(INPUT(_name="insertarConcepto", _type="submit", _class="btn btn-primary", _id="insertarConcepto", _value="Insertar"), _class="controls"), 
 								   _class="control-group")), 
 						_class="form-horizontal", _name="nuevoConcepto", _action=URL('/detalleFactura'))
-	if formConcepto.process().accepted:
+	if formConcepto.accepts(request, session):
 		info="Concepto insertado"
+		dbRosan.factura_detalle_temp.insert(id_factura=1, id_articulo=0, 
+											articulo=formConcepto.vars.descripcion, precio=formConcepto.vars.precio, cantidad=formConcepto.vars.cantidad, 
+											total_articulo=formConcepto.vars.precio * formConcepto.vars.cantidad)
+
 	elif formConcepto.errors:
 		info="Concepto erróneo"
 	else:
 		info="Rellena el formulario"
 
-	return dict(info=info, formConcepto=formConcepto)
+	formArticulo = FORM()
+
+	lineas = dbRosan().select(dbRosan.factura_detalle_temp.ALL)	
+	return dict(info=info, formConcepto=formConcepto, lineas=lineas, formArticulo=formArticulo)
 
